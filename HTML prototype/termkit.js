86d244e94b017e1cba5756edb462008fe706212c
Steven Wittens <steven@acko.net>
2010-03-11 02:21:49 -0800

    Refactor inputMgr into separate objects

--- HTML prototype/termkit.js
+++ HTML prototype/termkit.js
@@ -2,6 +2,23 @@ var termkit = {};
 
 (function ($) {
   
+///////////////////////////////////////////////////////////////////////////////
+
+function async_callback(func) {
+  return function () { 
+    var self = this;
+    var args = arguments;
+    setTimeout(function () { func.apply(self, args); }, 0);
+  };
+}
+
+function async(func) {
+  var self = this;
+  setTimeout(function () { func.call(self); }, 0);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 $.fn.termkitTokenField = function (options) {
   var $container = this;
 
@@ -18,24 +35,27 @@ $.fn.termkitTokenField = function (options) {
   var input = new termkit.inputManager($container[0]);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+
 termkit.inputManager = function (field) {
+  var self = this;
   var $field = this.$field = $(this.field = field);
-  var input = this;
+  
+  this.tokens = [];
+  this.caret = new termkit.inputManager.caret();
   
   // Set field event handlers.
-  $field.mousedown(function (e) { input.fieldMouseDown(e); });
+  $field.mousedown(function (e) { self.fieldMouseDown(e); });
   
 };
 
 termkit.inputManager.prototype.fieldMouseDown = function (event) {
   var $target = $(event.target);
-  if ($target.is('span.token')) {
-    this.focus($target);
-  }
-  else if (!$target.is('span.measure')) {
-    this.focus();
+  if ($target.is('.termkitTokenField')) {
+    this.caret.insertInto($target);
   }
   event.stopPropagation();
+  event.preventDefault();
 };
 
 termkit.inputManager.prototype.focus = function ($token) {
@@ -59,63 +79,85 @@ termkit.inputManager.prototype.blur = function () {
   this.debug();
 };
 
+///////////////////////////////////////////////////////////////////////////////
 
-termkit.inputManager.prototype.getCaret = function ($token) {
-  var $caret = $('<span class="caretWrapper" id="caret"><input class="caret" /><span class="measure"></span></span>');
-  var input = this;
+termkit.inputManager.caret = function () {
+  this.$caret = this.getElement();
+  this.$input = this.$caret.find('input');
+  this.$measure = this.$caret.find('.measure');
+};
+
+termkit.inputManager.caret.prototype.getElement = function () {
+  var $caret = $('<span id="caret"><input /><span class="measure"></span></span>');
+  var self = this;
   $caret.find('input')
-    .mousedown(function (e) { input.caretMouseDown(e); })
-    .keydown(function (e) { input.caretKeyDown(e); })
-    .blur(function (e) { input.blur(); });
+    .keydown(function (e) { self.onKeyDown(e); })
+    .keypress(function (e) { self.onKeyPress(e); })
+    .blur(function (e) { self.onBlur(); });
   return $caret;
 };
 
-termkit.inputManager.prototype.insertCaret = function ($token) {
-  var $caret = this.getCaret($token);
-  if ($token) {
-    var contents = $token.text();
-    $caret.find('input').val(contents);
-    $token.empty().append($caret);
+termkit.inputManager.caret.prototype.insertInto = function (element) {
+  this.remove();
+  $(element).append(this.$caret);
+  this.$input.focus();
+};
+
+termkit.inputManager.caret.prototype.remove = function () {
+  this.$caret.detach();
+};
+
+termkit.inputManager.caret.prototype.onKeyDown = function (event) {
+
+};
+
+termkit.inputManager.caret.prototype.onKeyPress = function (event) {
+  async.call(this, function () {
+    this.$measure.text(this.$input.val());
+  });
+};
+
+termkit.inputManager.caret.prototype.onBlur = function (element) {
+  this.remove();
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+termkit.inputManager.tokenFactory = function (contents) {
+  var sigil = contents.substring(0, 1);
+  switch (sigil) {
+    case '"':
+    case "'":
+      return new termkit.inputManager.tokenQuoted(contents);
+    default:
+      return new termkit.inputManager.token(contents);
   }
-  else {
-    this.$field.append($caret);
-  }
-  this.caretAutoSize();
-  setTimeout(function () { $('#caret input').focus(); }, 0);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+termkit.inputManager.token = function (contents) {
+  this.type = 'unknown';
+  this.contents = contents;
 };
 
-termkit.inputManager.prototype.removeCaret = function () {
-  var $caret = $('#caret');
-  var text = $caret.length && $caret.find('input').val();
-  if (text != '') {
-    if ($caret.parents('span.token').length == 0) {
-      text = '<span class="token">'+ text +'</span>';
-    }
-    $caret.before(text);
-  }
-  $caret.remove();
+termkit.inputManager.token.prototype.getElement = function () {
+  var $token = $('<span>').addClass('token-'+ this.type).text(this.contents);
+  var self = this;
+  $token//.bind events;
+  return $token;
 };
 
-termkit.inputManager.prototype.caretAutoSize = function () {
-  var $caret = $('#caret'),
-      $measure = $caret.find('span.measure'),
-      $input = $caret.find('input.caret');
-  $measure.text($input.val());
-  $input.css('width', $measure.width() + 20);
+termkit.inputManager.tokenQuoted = function (contents) {
+  this.type = 'quoted';
+  this.contents = contents.substring(1);
 };
 
-termkit.inputManager.prototype.caretMouseDown = function (event) {
-  event.stopPropagation();
-};
+termkit.inputManager.tokenQuoted.prototype = new termkit.inputManager.token();
 
-termkit.inputManager.prototype.caretKeyDown = function (event) {
-  var input = this;
-  setTimeout(function () { input.caretAutoSize(); }, 0);
-};
 
-termkit.inputManager.prototype.debug = function () {
-  $('#debug').text(this.$field.html());
-};
+
+///////////////////////////////////////////////////////////////////////////////
 
 
 })(jQuery);

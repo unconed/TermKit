253083f26f91cab60612fa0d5a3ff8124ea007d8
Steven Wittens <steven@acko.net>
2010-11-15 23:50:28 -0800

    Remove build

--- Cocoa/TermKit/build/TermKit.build/Debug/TermKit.build/Script-B0494B22124369A600867892.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-html="$TARGET_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH/HTML"
-rm -rf "$html/socket.io"
-mkdir "$html/socket.io"
-cp -r ../../Node/socket.io-node/support/socket.io-client/* "$html/socket.io"

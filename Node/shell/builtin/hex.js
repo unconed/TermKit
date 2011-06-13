0786cacd234debcd5b1b66aa50c83d5d09c8f087
Steven Wittens <steven@acko.net>
2011-06-12 18:28:01 -0700

    Leftovers

---
+++ Node/shell/builtin/hex.js
@@ -0,0 +1,65 @@
+var view = require('view/view'),
+    reader = require('shell/reader'),
+    parseArgs = require('misc').parseArgs;
+
+exports.main = function (tokens, pipes, exit, environment) {
+  var out = new view.bridge(pipes.viewOut);
+
+  var args = parseArgs(tokens),
+      options = args.options;
+
+  // Buffered mime reader handler.
+  var handler = {
+    
+    /**
+     * Pipe open, headers found.
+     */
+    begin: function (headers) {
+      // Override content-type, output rest.
+      headers.set('Content-Type', [ 'application/octet-stream', { schema: 'termkit.hex' } ]);
+      pipes.dataOut.write(headers.generate());
+    },
+
+    /**
+     * Pipe data.
+     */
+    data: function (data) {
+      // Pipe out.
+      pipes.dataOut.write(data);
+    },
+    
+    /**
+     * Pipe closed.
+     */
+    end: function (exit) {
+      // Quit immediately.
+      exit();
+    },
+  };
+  
+  // Reader callbacks
+  var errors = 0;
+  // Open
+  function readerOpen() {
+    return handler;
+  };
+  // Close
+  function readerClose() {
+    exit(errors == 0);
+  };
+  // Error
+  function readerError(error) {
+    errors++;
+    out.print(error);
+  };
+
+  // Spawn appropriate reader.
+  if (files.length) {
+    // Spawn files reader.
+    var pipe = new reader.filesReader(files, readerOpen, readerClose, readerError);
+  }
+  else {  
+    // Spawn data reader.
+    var pipe = new reader.dataReader(pipes.dataIn, readerOpen, readerClose, readerError);
+  }
+};
